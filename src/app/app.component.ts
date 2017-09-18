2eaf87658d5e66d9f168b70da5cdd0e5c9d8f12e
Stephen Hazleton <shazleto@gmail.com>
2017-09-19 09:38:56 +1000

    adding background image example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,28 +13,37 @@ export class AppComponent {
   public title = 'angular2-signaturepad-demo';
   public form: FormGroup;
 
+  // for convenience as we don't have a QueryList.index
+  public secondSig: SignatureFieldComponent;
+
   @ViewChildren(SignatureFieldComponent) public sigs: QueryList<SignatureFieldComponent>;
   @ViewChildren('sigContainer1') public sigContainer1: QueryList<ElementRef>;
   @ViewChildren('sigContainer2') public sigContainer2: QueryList<ElementRef>;
+  @ViewChildren('sigContainer3') public sigContainer3: QueryList<ElementRef>;
 
   constructor(fb: FormBuilder) {
 
     this.form = fb.group({
       signatureField1: ['', Validators.required],
-      signatureField2: ['', Validators.required]
+      signatureField2: ['', Validators.required],
+      signatureField3: ['', Validators.required]
     });
+
+    window['app'] = this;
   }
 
   public ngAfterViewInit() {
+    this.secondSig = this.sigs.find((sig, index) => index === 1);
     this.beResponsive();
     this.setOptions();
   }
 
   // set the dimensions of the signature pad canvas
   public beResponsive() {
-    console.log('Resizing signature pad canvas to suit container size')
+    console.log('Resizing signature pad canvas to suit container size');
     this.size(this.sigContainer1.first, this.sigs.first);
-    this.size(this.sigContainer2.first, this.sigs.last);
+    this.size(this.sigContainer2.first, this.secondSig);
+    this.size(this.sigContainer3.first, this.sigs.last);
   }
 
   public size(container: ElementRef, sig: SignatureFieldComponent){
@@ -44,19 +53,21 @@ export class AppComponent {
 
   public setOptions() {
     this.sigs.first.signaturePad.set('penColor', 'rgb(255, 0, 0)');
-    this.sigs.last.signaturePad.set('penColor', 'rgb(255, 255, 0)');
-    this.sigs.last.signaturePad.set('backgroundColor', 'rgb(0, 0, 255)');
-    this.sigs.last.signaturePad.clear(); // clearing is needed to set the background colour
+    this.secondSig.signaturePad.set('penColor', 'rgb(255, 255, 0)');
+    this.secondSig.signaturePad.set('backgroundColor', 'rgb(0, 0, 255)');
+    this.secondSig.signaturePad.clear(); // clearing is needed to set the background colour
   }
 
   public submit() {
     console.log('CAPTURED SIGS:');
     console.log(this.sigs.first.signature);
+    console.log(this.secondSig.signature);
     console.log(this.sigs.last.signature);
   }
 
   public clear() {
     this.sigs.first.clear();
+    this.secondSig.clear();
     this.sigs.last.clear();
   }
 }
